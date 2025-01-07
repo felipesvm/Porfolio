ebeae62c8e68ea80c9a977c76d0bf5849afd8157
felipesvm <federt49@gmail.com>
2025-01-07 08:21:27 -0300

    Novas atualizações

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ scrollAnima.init();
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addControl(".custom-controls");
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault(); // Impede o comportamento padrão do link
+
+    const targetId = this.getAttribute("href").substring(1); // Remove o '#'
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      // Rolagem suave até a seção
+      targetElement.scrollIntoView({ behavior: "smooth" });
+
+      // Atualiza a URL removendo o fragmento
+      history.pushState(null, null, window.location.pathname);
+    }
+  });
+});
